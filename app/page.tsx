cd4f6927ec457123104582c02143bfaf7e390534
VOLANT Samuel (CENTRE ADMINISTRATIF) <s.volant@adapei-nouelles.local>
2026-08-25 15:50:02 +0200

    Small error 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 import { Sparkles, Gift, Loader2 } from "lucide-react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
